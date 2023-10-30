90d1e54478aa82b53f09fe4b2c29f1fe096581e0
kylewang1999 <k5wang@ucsd.edu>
2023-10-30 10:48:33 -0700

    added support yaml configs

---
+++ config/load.py
@@ -0,0 +1,46 @@
+import yaml, torchvision.transforms as transforms
+from types import SimpleNamespace
+from datetime import datetime
+
+def dict_to_object(d):
+    if isinstance(d, dict): return SimpleNamespace(**{k: dict_to_object(v) for k, v in d.items()})
+    elif isinstance(d, list): return [dict_to_object(x) for x in d]
+    else: return d
+
+def replace_fstring_in_dict(d):
+    for key, value in d.items():
+        if isinstance(value, dict):
+            replace_fstring_in_dict(value)
+        elif isinstance(value, str):
+            d[key] = value.format_map(d)
+
+def merge_dicts(parent_dict, child_dict):
+    ''' Merge two dicts, child overwrites parent '''
+    merged_dict = {**parent_dict, **child_dict}
+    for key, value in parent_dict.items():
+        if isinstance(value, dict) and key in child_dict:
+            merged_dict[key] = {**value, **child_dict[key]}
+    return merged_dict
+
+def args_from_yaml(fname_base='./base.yaml', fname_aux=None) -> SimpleNamespace:
+    
+    conf = merge_dicts(
+        yaml.safe_load(open(fname_base, 'r')), 
+        yaml.safe_load(open(fname_aux, 'r'))
+    ) if fname_aux is not None else yaml.safe_load(open(fname_base, 'r'))
+
+    conf['exp']['date_time'] = datetime.now().strftime("%m%d-%H:%M")
+    args = dict_to_object(conf)
+
+    # Replace `base_dir`, `exp_name`, `date_time` in config fstrings
+    args.exp.log_dir= args.exp.log_dir.format_map(conf['exp'])
+    args.ctrnet.keypoint_seg_model_path = args.ctrnet.keypoint_seg_model_path.format_map(conf['exp'])
+    args.ctrnet.urdf_file = args.ctrnet.urdf_file.format_map(conf['exp'])
+    
+    # Set transformation for dataloader
+    args.r2d2.trans_to_tensor = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=args.r2d2.mean, std=args.r2d2.std),
+    ]) if args.r2d2.normalize else transforms.ToTensor()
+
+    return args
